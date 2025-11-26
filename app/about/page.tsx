b62066da44e1b36869c075315c6bfc4b14a21b6c
Emirhan <emirhanerkn@hotmail.com>
2025-11-26 20:50:30 +0300

    add and set Prisma to the project

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,8 +1,23 @@
-import HomePage from "@/app/page";
+import db from "@/utils/db";
+import { Key, ReactElement, JSXElementConstructor, ReactNode, ReactPortal } from "react";
 
-const AboutPage = () => {
+async function AboutPage() {
+    const products = await db.testProfile.create({
+        data: {
+            name: "Alice",
+        }
+    })
+    const users = await db.testProfile.findMany()
     return (
-        <div className="w-full">about</div>
+        <div>
+            {users.map((user: { id: Key | null | undefined; name: string | number | bigint | boolean | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | Promise<string | number | bigint | boolean | ReactPortal | ReactElement<unknown, string | JSXElementConstructor<any>> | Iterable<ReactNode> | null | undefined> | null | undefined; })=> {
+                return (
+                    <h2 key={user.id} className="text-2xl font-bold">
+                        {user.name}
+                    </h2>
+                )
+            })}
+        </div>
     )
 }
 export default AboutPage;
