f5eafd6d9497de46c68a8274d32c44ce73c157f5
Emirhan <emirhanerkn@hotmail.com>
2025-11-28 00:13:31 +0300

    feat(page/app): add Suspense for products when page loaded feat(LoadingContainer): add Skeleton Component to suspense for main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,15 @@
 import React from "react"
-import Button from "@mui/material/Button"
+import Hero from "@/components/home/Hero";
+import FeaturedProducts from "@/components/home/FeaturedProducts";
+import {Suspense} from "react";
+import LoadingContainer from "@/components/global/LoadingContainer";
 const HomePage = () => {
     return (
         <>
+          <Hero />
+            <Suspense fallback={<LoadingContainer/>}>
+                <FeaturedProducts />
+            </Suspense>
 
         </>
     )
