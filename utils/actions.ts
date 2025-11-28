6fa26337a451b82f365f8695e3020d72a4d405fd
Emirhan <emirhanerkn@hotmail.com>
2025-11-29 00:21:04 +0300

    feat(action): add where when search products in navsearch component

--- utils/actions.ts
+++ utils/actions.ts
@@ -10,8 +10,14 @@ export const fetchFeaturedProducts = async () => {
     return fetchData
 }
 
-export const fetchAllProducts = () => {
+export const fetchAllProducts = ({search=''}:{search: string}) => {
     return prisma.product.findMany({
+         where: {
+      OR: [
+        { name: { contains: search, mode: 'insensitive' } },
+        { company: { contains: search, mode: 'insensitive' } },
+      ],
+    },
         orderBy: {
             createdAt: 'desc'
         }
