a45fd6624cdf8c38592e6b69f8f8ae4370e9613d
Emirhan <emirhanerkn@hotmail.com>
2025-11-29 00:19:17 +0300

    feat(NavSearch): use usesearchparams, userouter for NavSearch Component when there is value in input, when change input value, change dynamic url like /products?search=lamb

--- components/navbar/NavSearch.tsx
+++ components/navbar/NavSearch.tsx
@@ -1,28 +1,45 @@
+"use client"
 import React from "react"
 import TextField from "@mui/material/TextField";
 import InputAdornment from "@mui/material/InputAdornment";
 import { CiSearch } from "react-icons/ci";
+import { useSearchParams, useRouter } from 'next/navigation';
+import { useDebouncedCallback } from 'use-debounce';
+import { useState, useEffect } from 'react';
+import { Input } from '../ui/input';
 const NavSearch = () => {
+    const searchParams = useSearchParams();
+    const router = useRouter();
+    const [searchTerm,setSearchTerm] = useState(searchParams.get('search')?.toString || '')
+   
+     const handleSearch = useDebouncedCallback((value: string)=>{
+        const params = new URLSearchParams(searchParams);
+        if (value) {
+            params.set('search', value);
+        }
+        else {
+            params.delete('search');
+        }
+        router.push(`/products?${params.toString()}`)
+     },300)
+
+     useEffect(()=>{
+         if(!searchParams.get('search')) {
+            setSearchTerm('');
+         }
+     },[searchParams.get('search')]);
+
     return (
-        <TextField
-            type="search"
-            variant="standard"
-            placeholder="Search Products..."
-            className="max-w-md w-full dark:bg-gray-800 dark:text-white"
-            InputProps={{
-                disableUnderline: true,
-                startAdornment: (
-                    <InputAdornment position="start">
-                      <CiSearch size={20}/>
-                    </InputAdornment>
-                ),
-            }}
-            sx={{
-                background: "#f0f0f0",
-                padding: "4px 12px",
-                borderRadius: "8px",
-            }}
-        />
+        <Input
+      type='search'
+      placeholder='search product...'
+      className='max-w-xs dark:bg-muted '
+      onChange={(e) => {
+        setSearchTerm(e.target.value);
+        handleSearch(e.target.value);
+      }}
+      value={searchTerm}
+    />
 
     )
 }
