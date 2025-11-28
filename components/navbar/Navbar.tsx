02cb7ae4669158d87338e5a8d487bda308766319
Emirhan <emirhanerkn@hotmail.com>
2025-11-29 00:20:03 +0300

    fix(Navbar): add Suspense for NavSearch component

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -4,13 +4,16 @@ import NavSearch from "@/components/navbar/NavSearch";
 import CartButton from "@/components/navbar/CartButton";
 import DarkMode from "@/components/navbar/DarkMode";
 import LinksDropdown from "@/components/navbar/LinksDropdown";
+import { Suspense } from "react";
 
 const Navbar = () => {
     return (
        <nav className="border-b">
            <Container className="flex flex-col sm:flex-row sm:justify-between sm:items-center flex-wrap py-8 gap-4">
             <Logo/>
+                <Suspense>
                <NavSearch/>
+               </Suspense>
                <div className="flex gap-4 items-center">
                  <CartButton/>
                  <DarkMode/>
