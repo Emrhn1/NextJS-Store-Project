f7e4157a1e83bd78c36e22e723dd80096bee1d1d
Emirhan <emirhanerkn@hotmail.com>
2025-11-30 00:59:57 +0300

    feat(Productrating): add productrating component for product details page

---
+++ components/single-product/ProductRating.tsx
@@ -0,0 +1,14 @@
+import { FaStar } from 'react-icons/fa';
+
+function ProductRating({productId}: {productId: string}) {
+    const rating = 4.2;
+    const count = 25;
+    const countValue = `(${count}) reviews`;
+    return (
+        <span className="flex gap-1 items-center text-md mt-1 mb-4">
+            <FaStar className='w-3 h-3' />
+            {rating} {countValue}
+        </span>
+    )
+}
+export default ProductRating;
