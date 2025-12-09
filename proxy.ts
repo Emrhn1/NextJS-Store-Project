d69ef2effea490b15ad9f80663d1464bf5b6c403
Emirhan <emirhanerkn@hotmail.com>
2025-12-09 12:14:04 +0300

    feat(proxy): add and set admin pages. if admin, access all admin pages. if is not admin, return / url

--- proxy.ts
+++ proxy.ts
@@ -1,12 +1,18 @@
 // proxy.ts
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
+import { NextResponse } from "next/server";
 
 const isPublicRoute = createRouteMatcher(['/', '/about', '/products(.*)']);
-
+const isAdminRoute = createRouteMatcher(['/admin(.*)'])
 export default clerkMiddleware(async (auth, req) => {
+  const session = await auth();
+  const isAdminUser = session.userId === process.env.ADMIN_USER_ID
+  if (isAdminRoute(req) && !isAdminUser) {
+    return NextResponse.redirect(new URL('/', req.url))
+  }
   if (isPublicRoute(req)) return;
 
-  const session = await auth();
+
 
   if (!session.userId) {
     return session.redirectToSignIn({ returnBackUrl: req.url });
@@ -19,3 +25,4 @@ export const config = {
     '/(api|trpc)(.*)',
   ],
 };
+
