0db9b4c5633e80171e0fec9769a7310fc2f00bce
Emirhan <emirhanerkn@hotmail.com>
2025-12-12 15:10:13 +0300

    feat(createproductspage): add FormContainer and them components in page

--- app/admin/products/create/page.tsx
+++ app/admin/products/create/page.tsx
@@ -1,24 +1,35 @@
 "use client"
-import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
-import { Input } from "@/components/ui/input";
-const createProductAction = async (formData: FormData) => {
-    const name = formData.get("name") as string;
-    console.log(name)
-}
+import FormInput from "@/components/form/FormInput";
+import {type} from "node:os";
+import FormContainer from "@/components/form/FormContainer";
+import {createProductAction} from "@/utils/actions";
+import PriceInput from "@/components/form/PriceInput";
+import ImageInput from "@/components/form/ImageInput";
+import TextAreaInput from "@/components/form/TextAreaInput";
+import CheckboxInput from "@/components/form/CheckBoxInput";
+import Buttons from "@/components/form/Buttons";
 
 function CreateProductPage() {
     return (
         <section>
             <h1 className="text-2xl font-semibold mb-8 capitalize">Create Product</h1>
             <div className="border p-8 rounded-md">
-                <form action={createProductAction}>
-                    <div className="mb-2">
-                        <Label className="mb-4" htmlFor="name">Product Name</Label>
-                        <Input className="mb-4" id="id" name="name" type="text" />
+                <FormContainer action={createProductAction}>
+                         <div className="grid gap-4 md:grid-cols-2 my-4">
+                            <FormInput name="name" type="text"
+                            label="Product Name"
+                            />
+                             <FormInput name="company" type="text" label="company" defaultValue="company"/>
+                             <PriceInput/>
+                             <ImageInput/>
+                         </div>
+                      <TextAreaInput name="description" labelText="Product Description" defaultValue="Product Description"/>
+                    <div className="mt-6">
+                        <CheckboxInput name="featured" label="featured"/>
                     </div>
-                    <Button size="lg" type="submit">Submit</Button>
-                </form>
+                    <Buttons className="mt-4" text="Create Product" />
+                </FormContainer>
             </div>
         </section>
     )
