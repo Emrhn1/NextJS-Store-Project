63c04d9bce1d478f0fedc3a11c1147276c9013de
Emirhan <emirhanerkn@hotmail.com>
2025-12-12 15:00:44 +0300

    feat(action) : add createProduct action function, get all prouduct properties from the formData and in the CreateProductPage, create the new product if user is not null. handle error function

--- utils/actions.ts
+++ utils/actions.ts
@@ -1,5 +1,16 @@
+"use server"
+
 import prisma from "@/utils/db";
 import {redirect} from "next/navigation";
+import {currentUser} from "@clerk/nextjs/server";
+import {RenderError} from "next/dist/next-devtools/dev-overlay/container/runtime-error/render-error";
+
+const renderError = (error: unknown): { message: string } => {
+    console.log(error);
+    return {
+        message: error instanceof Error ? error.message : 'An error occurred',
+    };
+};
 
 export const fetchFeaturedProducts = async () => {
     const fetchData = await prisma.product.findMany({
@@ -10,7 +21,7 @@ export const fetchFeaturedProducts = async () => {
     return fetchData
 }
 
-export const fetchAllProducts = ({search=''}:{search: string}) => {
+export const fetchAllProducts = async ({search=''}:{search: string}) => {
     return prisma.product.findMany({
          where: {
       OR: [
@@ -34,4 +45,33 @@ export const fetchSingleProduct = async (productId: string) => {
         redirect('products')
     }
     return product;
+}
+
+export const createProductAction = async (prevState: any, formData: FormData): Promise<{message:string}> => {
+    const user = await currentUser()
+    if(!user) redirect("/")
+    try {
+      const name = formData.get("name") as string;
+      const company = formData.get("company") as string;
+      const price = Number(formData.get("price") as string);
+      const image = formData.get("image") as File;
+      const featured = Boolean(formData.get("featured") as string);
+      const description = formData.get("description") as string;
+
+      await prisma.product.create({
+          data: {
+              name,
+              company,
+              price,
+              image:"/images/product-01.jpg",
+              featured,
+              description,
+              clerkId: user.id,
+          }
+      })
+
+      return {message: "Product Created"}
+    } catch (error) {
+        return renderError(error);
+    }
 }
